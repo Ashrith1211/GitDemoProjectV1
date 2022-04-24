81ac31a3c6cd7b0693519f11ba8072e991ab0113
M1083047 <M1083047@MT-5CG1418SM5>
2022-04-24 14:27:59 +0530

    First Commit....

---
+++ src/Test1.java
@@ -0,0 +1,14 @@
+
+public class Test1 {
+	public static void main(String[] aegs) {
+		String hungry = "yes";
+		if(hungry == "yes") 
+			System.out.println("eat something....");
+			else {
+				System.out.println("eat something.....");
+				
+			}
+		}
+	}
+
+
